6facb84b045a91fb9df5f993d42f106dc7d2bc81
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-12-22 22:44:33 -0300

    number
    tratamento de números inteiros, e correção de números com pontos flutuantes

---
+++ javaScriptBasico/javaScript/numbers.js
@@ -0,0 +1,18 @@
+//IEEE 754-2008 padrão numérico 
+
+let num1 = 0.7; 
+let num2 = 0.1;
+num1 += num2 //.8
+num1 += num2 //.9
+num1 += num2 //1.0
+
+//num1 = parseFloat(num1.toFixed(2)); // para corigir a imprecisão de números com pontos flutuantes
+//num1 = Number(num1.toFixed(2)); // para corigir a imprecisão de números com pontos flutuantes
+//console.log(num1)
+//console.log(Number.isInteger(num1));
+//console.log(num1.toString() + num2); //transforma um number em string
+//console.log(num1.toString(2)); // para mostrar o código binario
+//console.log(num1.toFixed(2)); // para mostrar apenas a quantidade desejada após a vírgula
+//console.log(Number.isInteger(num1)); // para saber se o número é inteiro
+//let temp = num1 * "oi";
+//console.log(Number.isNaN(temp)); //para saber se a conta é inválida not a number NaN/
