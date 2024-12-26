23b3431830f0a1f20671929c0470de6d0c682eae
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-12-25 21:32:04 -0300

    Arrays
    Criação e manipulação de arrays, alteração de valores e índices, remoção, implementação e alteração de valores e elementos nas arrays.

---
+++ javaScriptBasico/javaScript/arrays(basico).js
@@ -0,0 +1,37 @@
+//indice           0       1        2
+const alunos = ["Luiz", "Maria", "João"];
+
+//console.log(alunos);
+//console.log(alunos[0]); // busca o indice
+//console.log(alunos[2]); // busca o indice
+
+//alunos[0] = "Eduardo"; // altera o indice 
+//alunos[3] = "Luiza"; // altera o indice 
+
+//alunos[alunos.length] = "Luiza";// adiciona um elemento após o ultimo indice
+//alunos[alunos.length] = "Fábio";// adiciona um elemento após o ultimo indice
+//alunos[alunos.length] = "Luana"; // adiciona um elemento após o ultimo indice
+
+//alunos.push("Luiza"); // adiciona o elemento no final do array
+//alunos.push("Fábio"); // adiciona o elemento no final do array
+
+//alunos.unshift("Luiza"); //adiciona o elemento no começo do array
+//alunos.unshift("Fábio"); //adiciona o elemento no começo do array
+
+//alunos.pop(); // remove o ultimo elemento
+//const removido = alunos.pop(); // remove o ultimo elemento adicionando ele em uma variável
+//console.log(removido)
+
+//alunos.shift(); // remove o primeiro elemento
+//const removido = alunos.shift(); // remove o primeiro elemento adicionando ele em uma variável
+//console.log(removido)
+
+//delete alunos[1]; // deleta apenas o valor do indice mantendo o indice vazio
+
+//console.log(alunos.slice(0, 2)); // busca pelos elemento em um intervalo especificado
+//console.log(alunos.slice(0, -2)); // busca pelos elemento em um intervalo especificado(final)
+
+//console.log(typeof alunos); //object(objeto)
+//console.log(alunos instanceof Array) // para saber se realmente é um array
+
+console.log(alunos)
