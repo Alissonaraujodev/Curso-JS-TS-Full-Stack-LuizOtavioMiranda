95d51097700644a5d6e0e0186acd679abd265831
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2024-12-26 22:03:37 -0300

    funções
    Tipos de funções, nomeadas, não nomeadas(dentro de uma variável), com parâmetros e sem parâmetros, arrow function.

---
+++ javaScriptBasico/javaScript/funcoes(basico).js
@@ -0,0 +1,49 @@
+ /*                 
+function saudacao(nome){
+    return `Bom dia ${nome}.`
+}
+const variavel = saudacao("Luiz")
+console.log(variavel)
+*/
+
+/*
+function soma(x = 1, y = 1){
+    const resultado = x + y;
+    return resultado;
+}
+
+//console.log(soma(2, 2))
+//console.log(soma(3, 1))
+//console.log(soma(5, 10))
+
+const resultado = soma(4, 2);
+console.log(resultado);
+*/
+
+/*
+const raiz = function(n){
+    return n ** 0.5
+};
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+*/
+
+/*
+const raiz = (n) =>  {  //arrow funcition
+    return n ** 0.5; 
+}
+
+console.log(raiz(9))
+console.log(raiz(16))
+console.log(raiz(25))
+*/
+
+/*
+const raiz = (n) =>  n ** 0.5 // arrow function com apenas uma sentença
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
+*/
