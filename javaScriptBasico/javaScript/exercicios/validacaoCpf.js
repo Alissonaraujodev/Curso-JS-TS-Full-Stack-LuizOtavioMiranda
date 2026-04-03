6c5045f4d0dcaaa4845b1659392765efaa28a29a
Alisson Araújo Mariano <157244142+Alissonaraujodev@users.noreply.github.com>
2026-04-03 19:11:51 -0300

    Update validacaoCpf.js

--- javaScriptBasico/javaScript/exercicios/validacaoCpf.js
+++ javaScriptBasico/javaScript/exercicios/validacaoCpf.js
@@ -1,17 +1,34 @@
-let cpf = '466.968.268';
-let cpfLimpo = cpf.replace(/\D+/g, '');
-let cpfArray = Array.from(cpfLimpo);
+//let cpf = '466.968.268-74';
+//let cpfLimpo = cpf.replace(/\D+/g, '');
+//let cpfArray = Array.from(cpfLimpo);
+//let tirarDigitosCpf = cpfArray.splice(-2, 2);
 
-const primeiroDigito = cpfArray
-    .map((valor, index) => valor * (10 - index))
-    .reduce((acumulador, valor) => acumulador + valor) * 10 % 11;
 
-cpfArray.push(primeiroDigito)
+function ValidarCpf(cpf){
+    this.cpfLimpo = cpf.replace(/\D+/g, '');
+    this.cpfArray = Array.from(this.cpfLimpo).map(Number);
+    this.tirarDigitosCpf = this.cpfArray.splice(-2, 2);
 
-const segundoDigito = cpfArray
-    .map((valor, index) => valor * (11 - index))
-    .reduce((acumulador, valor) => acumulador + valor) * 10 % 11;
+    this.gerarDigito = (array, multiplicadorInicial) => {
+        const soma = array
+            .map((valor, index) => valor * (multiplicadorInicial - index))
+            .reduce((acumulador, valor) => acumulador + valor);
+        
+        const resto = 11 - (soma % 11);
+        return resto > 9 ? 0 : resto;
+    };
 
-cpfArray.push(segundoDigito)
+    const d1 = this.gerarDigito(this.cpfArray, 10);
+    this.cpfArray.push(d1);
 
-console.log(cpfArray.toString())
+    const d2 = this.gerarDigito(this.cpfArray, 11);
+    this.cpfArray.push(d2);
+
+    this.novoCpf = this.cpfArray.join('');
+    this.valido = () => this.novoCpf === this.cpfLimpo;
+
+}
+
+const cpf = new ValidarCpf('070.987.720-03')
+console.log(cpf.valido())
+
